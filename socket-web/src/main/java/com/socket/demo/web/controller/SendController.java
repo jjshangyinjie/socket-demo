2f7a7eb55177262dc42d34b23ddbc1897dfcb86e
Shang Yinjie <shangyinjie880613@126.com>
2017-06-04 23:42:55 +0800

    init

---
+++ socket-web/src/main/java/com/socket/demo/web/controller/SendController.java
@@ -0,0 +1,26 @@
+package com.socket.demo.web.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Shang Yinjie on 2017/6/4.
+ */
+@Controller
+@RequestMapping("/")
+public class SendController {
+
+    private static final Log log = LogFactory.getLog(SendController.class);
+
+    @RequestMapping("/onMessage")
+    public String onMessage(Model view) {
+        log.info("start send message!");
+
+        view.addAttribute("name", "onMessage");
+        return "socket/tables";
+    }
+
+}
